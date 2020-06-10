d823f5b65846e4919825315a06990fb3cd4567d7
georgantas <18753033+Georgantas@users.noreply.github.com>
2020-06-10 17:38:35 -0400

    prog

---
+++ GreetingMojo.java
@@ -0,0 +1,29 @@
+
+package sample.plugin;
+
+// The class org.apache.maven.plugin.AbstractMojo provides most
+// of the infrastructure required to implement a mojo except for
+// the execute method.
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+    
+/**
+ * Says "Hi" to the user.
+ *
+ */
+@Mojo( name = "sayhi")
+public class GreetingMojo extends AbstractMojo
+{
+    public void execute() throws MojoExecutionException
+    {
+        // org.apache.maven.plugin.MojoExecutionException if an unexpected
+        // problem occurs. Throwing this exception causes a "BUILD ERROR"
+        // message to be displayed.
+        getLog().info( "Hello, world." ); // retrieve the mojo logger
+
+        // throw org.apache.maven.plugin.MojoFailureException if an
+        // expected problem (such as a compilation failure) occurs.
+        // Throwing this exception causes a "BUILD FAILURE" message to be displayed.
+    }
+}
